249c58932b8b37d19dde565c2ef7f309a8d18642
Danny Lagrouw <danny.lagrouw@gmail.com>
2015-03-18 22:19:46 +0100

    Handle clicks

--- public/js/nono.js
+++ public/js/nono.js
@@ -24,6 +24,10 @@ angular.module('nono', [])
       return new Cell(row, col, Math.random() < density ? CellStatus.FILLED : CellStatus.EMPTY, CellStatus.EMPTY);
     };
 
+    Cell.withStatus = function(originalCell, newStatus) {
+      return new Cell(originalCell.row, originalCell.col, originalCell.solution, newStatus);
+    };
+
     function Strip(coord, cells) {
       this.coord = coord;
       this.cells = cells;
@@ -86,7 +90,7 @@ angular.module('nono', [])
 
     }
 
-    function Board(size) {
+    function Board(size, transformation) {
       var self = this;
       this.size = size;
       var DENSITY = 0.4;
@@ -96,7 +100,11 @@ angular.module('nono', [])
         for (var row = 0; row < size; row++) {
           cells[row] = new Array(size);
           for (var col = 0; col < size; col++) {
-            cells[row][col] = Cell.withDensity(row, col, DENSITY);
+            if (transformation) {
+              cells[row][col] = transformation.getCell(row, col);
+            } else {
+              cells[row][col] = Cell.withDensity(row, col, DENSITY);
+            }
           }
         }
         return cells;
@@ -143,6 +151,17 @@ angular.module('nono', [])
       };
     }
 
+    function MoveTransformation(board, cellStatus, row, col) {
+      this.getCell = function(r, c) {
+        //console.log('transformation[' + row + ',' + col + '].getCell(' + r + ',' + c + ')');
+        if ((r === row) && (c === col)) {
+          return Cell.withStatus(board.cells[r][c], cellStatus);
+        } else {
+          return board.cells[r][c];
+        }
+      };
+    }
+
     function CanvasBoardRenderer(ctx) {
       var self = this;
       this.ctx = ctx;
@@ -157,7 +176,26 @@ angular.module('nono', [])
         this.leading = leading;
 
         this.draw = function() {
+          ctx.clearRect(cell.col * (CELL_SIZE + CELL_SPACING) + indent, cell.row * (CELL_SIZE + CELL_SPACING) + leading, CELL_SIZE, CELL_SIZE);
           ctx.strokeRect(cell.col * (CELL_SIZE + CELL_SPACING) + indent, cell.row * (CELL_SIZE + CELL_SPACING) + leading, CELL_SIZE, CELL_SIZE);
+          if (cell.status === CellStatus.CROSSED) {
+            ctx.fillRect(cell.col * (CELL_SIZE + CELL_SPACING) + indent, cell.row * (CELL_SIZE + CELL_SPACING) + leading, CELL_SIZE, CELL_SIZE);
+          } else if (cell.status === CellStatus.BLANKED) {
+            var x1 = cell.col * (CELL_SIZE + CELL_SPACING) + indent;
+            var x2 = x1 + CELL_SIZE;
+            var y1 = cell.row * (CELL_SIZE + CELL_SPACING) + leading;
+            var y2 = y1 + CELL_SIZE;
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+            ctx.closePath();
+            ctx.beginPath();
+            ctx.moveTo(x2, y1);
+            ctx.lineTo(x1, y2);
+            ctx.stroke();
+            ctx.closePath();
+          }
         };
 
         this.drawSolution = function() {
@@ -167,6 +205,24 @@ angular.module('nono', [])
           }
         };
       }
+
+      this.toCol = function(board, x) {
+        var rows = board.getRows();
+        var maxRowGroups = rows.getMaxGroupCount();
+        var indent = CELL_SIZE * (maxRowGroups + 1);
+        var col = Math.floor((x - indent) / (CELL_SIZE + CELL_SPACING));
+        console.log('toCol ' + x + ' -> ' + col);
+        return col;
+      };
+
+      this.toRow = function(board, y) {
+        var columns = board.getColumns();
+        var maxColumnGroups = columns.getMaxGroupCount();
+        var leading = CELL_SIZE * (maxColumnGroups + 1);
+        var row = Math.floor((y - leading) / (CELL_SIZE + CELL_SPACING));
+        console.log('toRow ' + y + ' -> ' + row);
+        return row;
+      };
 
       this.renderSolution = function(board) {
         this.render(board, function(canvasCell) {
@@ -206,18 +262,18 @@ angular.module('nono', [])
         var columns = board.getColumns();
         var maxRowGroups = rows.getMaxGroupCount();
         var maxColumnGroups = columns.getMaxGroupCount();
-        var indent = 30 * (maxRowGroups + 1);
-        var leading = 30 * (maxColumnGroups + 1);
+        var indent = CELL_SIZE * (maxRowGroups + 1);
+        var leading = CELL_SIZE * (maxColumnGroups + 1);
 
         rows.strips.forEach(function(strip, stripIndex) {
-          strip.getGroupSizes().forEach(function(size, sizeIndex) {
-            ctx.fillText(size.toString(), sizeIndex * CELL_SIZE, (stripIndex + .5) * (CELL_SIZE + CELL_SPACING) + leading);
+          strip.getGroupSizes().forEach(function(size, sizeIndex, array) {
+            ctx.fillText(size.toString(), indent - ((array.length - sizeIndex) * CELL_SIZE), (stripIndex + .7) * (CELL_SIZE + CELL_SPACING) + leading);
           })
         });
 
         columns.strips.forEach(function(strip, stripIndex) {
-          strip.getGroupSizes().forEach(function(size, sizeIndex) {
-            ctx.fillText(size.toString(), (stripIndex + .5) * (CELL_SIZE + CELL_SPACING) + indent, (sizeIndex + 1) * CELL_SIZE);
+          strip.getGroupSizes().forEach(function(size, sizeIndex, array) {
+            ctx.fillText(size.toString(), (stripIndex + .3) * (CELL_SIZE + CELL_SPACING) + indent, leading - ((array.length - sizeIndex - .5) * CELL_SIZE));
           })
         });
 
@@ -231,19 +287,33 @@ angular.module('nono', [])
       };
     }
 
+    $scope.data = {
+      board: undefined,
+      renderer: undefined
+    };
+
     $scope.init = function() {
       var canvas = document.getElementById('cnv');
       var ctx = canvas.getContext('2d');
 
-      var board = new Board(10);
-      console.log(board.cells);
-      console.log(board.getColumns());
-      console.log(board.getColumns().getMaxGroupCount());
-      board.getColumns().strips.forEach(function(strip) {
+      $scope.data.board = new Board(10);
+      console.log($scope.data.board.cells);
+      console.log($scope.data.board.getColumns());
+      console.log($scope.data.board.getColumns().getMaxGroupCount());
+      $scope.data.board.getColumns().strips.forEach(function(strip) {
         console.log('strip ' + strip.coord + ': ' + strip.getGroupSizes());
       });
-      var renderer = new CanvasBoardRenderer(ctx);
-      renderer.renderSolution(board);
+      $scope.data.renderer = new CanvasBoardRenderer(ctx);
+      $scope.data.renderer.renderPlay($scope.data.board);
+    };
+
+    $scope.clicked = function(event) {
+      console.log('click ' + event.offsetX + ', ' + event.offsetY);
+      var col = $scope.data.renderer.toCol($scope.data.board, event.offsetX);
+      var row = $scope.data.renderer.toRow($scope.data.board, event.offsetY);
+      var status = (event.shiftKey ? CellStatus.BLANKED : (event.altKey ? CellStatus.EMPTY : CellStatus.CROSSED));
+      $scope.data.board = new Board($scope.data.board.size, new MoveTransformation($scope.data.board, status, row, col));
+      $scope.data.renderer.renderPlay($scope.data.board);
     };
 
   }]);
